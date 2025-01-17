2e1420c0f0f7f9c944c096808258ab8d4fa0544e
Annabelle Chen <150537808+annabchen@users.noreply.github.com>
2025-01-17 14:55:33 -0500

    Update rect_loop_0.sv

--- rectangle loop/rect_loop_0.sv
+++ rectangle loop/rect_loop_0.sv
@@ -8,12 +8,12 @@ module rect_loop
     output logic outM[ROW_LEN][COL_LEN]);
 
     // decoding instr to find num of loops
-    logic numloops = instr[11:0];
+    int numloops = instr[11:0];
     
     initial begin
       for (int i = 0; i < numloops; i++) begin
         // call swap locating module
-        swap_location sl(inM, outM);
+        swap_locate #(ROW_LEN, COL_LEN) sl(.inM(inM), .outM(outM));
         inM = sl;
       end
     end
@@ -30,26 +30,25 @@ module swap_locate
     output logic outM[ROW_LEN][COL_LEN]);
 
     // randomly generate r1 and c1
-    logic r1 = $urandom_range(0, ROW_LEN);
-    logic c1 = $urandom_range(0,COL_LEN);
+    int r1 = $urandom_range(0, ROW_LEN-1);
+    int c1 = $urandom_range(0, COL_LEN-1);
+    int r2 = $urandom_range(0, ROW_LEN-1);
+    int c2 = $urandom_range(0, COL_LEN-1);
 
-    // generating for r2 and c2
-    logic r2 = $urandom_range(0, ROW_LEN);
-    logic c2 = $urandom_range(0,COL_LEN);
-
+    // generating confirming values for r2 and c2
   initial begin
     while (inM[r1][c1] == inM[r1][c2]) begin
-      r2 = $urandom_range(0, ROW_LEN);
+      r2 = $urandom_range(0, ROW_LEN-1);
     end
   end
 
   initial begin
     while(inM[r1][c1] != inM[r2][c2]) begin
-      c2 = $urandom_range(0, COL_LEN);
+      c2 = $urandom_range(0, COL_LEN-1);
     end
   end
   // call to swapping module
-  swapping s(inM, r1, r2, c1, c2, outM);
+  swapping #(ROW_LEN, COL_LEN) s(.inM(inM), .r1(r1), .r2(r2), .c1(c1), .c2(c2), .outM(outM));
 endmodule
 
 
@@ -59,15 +58,16 @@ module swapping
   #(parameter
     ROW_LEN = 4,
     COL_LEN = 4) 
-   (input logic inM[ROW_LEN][COL_LEN],
-    input logic [19:0] r1, // assuming the 20-bit is the max length of the matrix
-    input logic [19:0] r2,
-    input logic [19:0] c1,
-    input logic [19:0] c2,
-    output logic outM[ROW_LEN][COL_LEN]);
+  (input logic [19:0] inM[ROW_LEN][COL_LEN], // assuming the 20-bit is the max length of the matrix
+    input int r1, 
+    input int r2,
+    input int c1,
+    input int c2,
+   output logic [19:0] outM[ROW_LEN][COL_LEN]);
+
+  outM = inM; // start w original matrix
   
   if(inM[r1][c2] == inM[r2][c1]) begin // just testing for this bc we already know that (r2,c2) has the same value as (r1,c2) and (r1, c2) != (r1,c1)
-    outM = inM;
     // XOR the original values to flip them
     outM[r1][c1] = ^(inM[r1][c1]);
     outM[r1][c2] = ^(inM[r1][c2]);
@@ -75,6 +75,3 @@ module swapping
     outM[r2][c2] = ^(inM[r2][c2]);
   end
 endmodule
-
-  
-  
